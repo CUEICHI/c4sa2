067973dff6f835e7704dd9986be5feed766ce1e2
HADA Hisakazu <hada@sfc.keio.ac.jp>
2014-06-28 17:53:26 +0900

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 C4sa::Application.routes.draw do
   resources :tweets
 
+  root 'tweets#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
